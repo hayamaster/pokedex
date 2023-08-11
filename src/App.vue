e79fc543f2e4772540b0d831fd84c026e3d584e6
hayamaster <dnfxmfkdbzos@naver.com>
2023-08-11 11:38:55 +0900

    create pokedex clone coding

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,74 @@
 <template>
   <div>
-    <h1>
-      {{ msg }}
-    </h1>
-    <Hello />
+    <header class="header">
+      <label>
+        Type pokemon name or ID:
+        <input type="text" v-model="pokemonID" />
+        <button class="searchBtn" @click="searchPokemon">
+          Search pokemon!
+        </button>
+      </label>
+    </header>
+
+    <main class="main" v-if="Object.entries(pokemonData).length > 0">
+      <section class="pokemonCard">
+        <div class="nameImage">
+          <h1 class="pokemonName">{{ pokemonData.name }}</h1>
+          <img
+            :src="pokemonData.sprites.front_default"
+            :alt="pokemonData.name"
+          />
+        </div>
+        <ul class="type">
+          <h2>Type:</h2>
+          <li
+            v-for="(type, index) in pokemonData.types"
+            :key="index"
+            :class="type.type.name"
+          >
+            <span>{{ type.type.name }}</span>
+          </li>
+        </ul>
+        <ul class="stats">
+          <h2>Stats:</h2>
+          <li v-for="(stat, index) in pokemonData.stats" :key="index">
+            <span>{{ stat.stat.name }} -> {{ stat.base_stat }}</span>
+          </li>
+        </ul>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import Hello from '~/components/Hello';
+import { pokemonApi } from "~/api/pokemonApi";
 
 export default {
-  components: {
-    Hello,
-  },
+  name: "App",
+
   data() {
     return {
-      msg: 'Hello Webpack',
+      pokemonData: {},
+      pokemonID: "",
     };
+  },
+  methods: {
+    async searchPokemon() {
+      try {
+        const pokemon = await fetch(`${pokemonApi}/${this.pokemonID}`).then(
+          (res) => res.json()
+        );
+        this.pokemonData = pokemon;
+        console.log(pokemon);
+        return pokemon;
+      } catch (error) {
+        alert("Pokemon was not found!!!");
+      }
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../style/style.scss";
+</style>
